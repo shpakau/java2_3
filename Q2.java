e58e8f8986ebdcc07e3bfc8134e837330d10374e
shpakau <51748821+shpakau@users.noreply.github.com>
2023-02-21 09:19:43 +0300

    Update Q2.java

--- Q2.java
+++ Q2.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 
-public class hw3task2 {
+public class task2 {
     public static void main(String[] args) {
         int[] myList = {8, 72, 43, 6, 1, 13, 18, 76, 33, 59};
         ArrayList<Integer> newlist = new ArrayList<Integer>();
